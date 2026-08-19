09a446240e9ec4308cd09a0432ad842afb36fba5
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-19 23:40:23 +0200

    🐛 Fixed poseidon trident using a wrong rotation on multiple display contexts

--- neoforge/src/main/java/dev/xylonity/olympus/client/item/renderer/PoseidonTridentItemRenderer.java
+++ neoforge/src/main/java/dev/xylonity/olympus/client/item/renderer/PoseidonTridentItemRenderer.java
@@ -1,13 +1,47 @@
 package dev.xylonity.olympus.client.item.renderer;
 
+import com.geckolib.animatable.GeoItem;
+import com.geckolib.constant.DataTickets;
 import com.geckolib.renderer.GeoItemRenderer;
+import com.geckolib.renderer.base.GeoRenderState;
+import com.geckolib.renderer.base.RenderPassInfo;
+import com.mojang.math.Axis;
 import dev.xylonity.olympus.client.item.model.PoseidonTridentModel;
 import dev.xylonity.olympus.common.item.PoseidonTridentItem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.item.ItemDisplayContext;
 
 public final class PoseidonTridentItemRenderer extends GeoItemRenderer<PoseidonTridentItem> {
 
     public PoseidonTridentItemRenderer() {
         super(new PoseidonTridentModel<>());
+    }
+
+    @Override
+    public void adjustRenderPose(final RenderPassInfo<GeoRenderState> renderPassInfo) {
+        final LocalPlayer player = Minecraft.getInstance().player;
+        final ItemDisplayContext context = renderPassInfo.getGeckolibData(DataTickets.ITEM_RENDER_PERSPECTIVE);
+        final long stackId = renderPassInfo.getOrDefaultGeckolibData(DataTickets.ANIMATABLE_INSTANCE_ID, Long.MIN_VALUE);
+
+        if (player != null) {
+            if (player.isUsingItem() && GeoItem.getId(player.getUseItem()) == stackId) {
+                if (context == ItemDisplayContext.THIRD_PERSON_RIGHT_HAND || context == ItemDisplayContext.THIRD_PERSON_LEFT_HAND) {
+                    renderPassInfo.poseStack().mulPose(Axis.XP.rotationDegrees(180));
+                    renderPassInfo.poseStack().translate(0, -1.25, -1);
+                }
+                else if (context == ItemDisplayContext.FIRST_PERSON_RIGHT_HAND) {
+                    renderPassInfo.poseStack().translate(0.25, -0.4, -0.4);
+                }
+                else if (context == ItemDisplayContext.FIRST_PERSON_LEFT_HAND) {
+                    renderPassInfo.poseStack().translate(-0.25, -0.4, -0.4);
+                }
+
+            }
+
+        }
+
+        super.adjustRenderPose(renderPassInfo);
     }
 
 }
