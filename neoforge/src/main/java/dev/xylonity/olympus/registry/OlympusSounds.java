4da825cae1d5dbf4c19a71f495d97ba74421a753
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-21 17:10:46 +0200

    ☔ Sound for the trident hit

--- neoforge/src/main/java/dev/xylonity/olympus/registry/OlympusSounds.java
+++ neoforge/src/main/java/dev/xylonity/olympus/registry/OlympusSounds.java
@@ -11,5 +11,6 @@ public final class OlympusSounds {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(Registries.SOUND_EVENT, Olympus.MOD_ID);
 
     public static final DeferredHolder<SoundEvent, SoundEvent> ZEUS_BRACERS_LIGHTNING_STRIKE = SOUND_EVENTS.register("zeus_bracers_lightning_strike", () -> SoundEvent.createVariableRangeEvent(Olympus.of("zeus_bracers_lightning_strike")));
+    public static final DeferredHolder<SoundEvent, SoundEvent> POSEIDONS_TRIDENT_HIT = SOUND_EVENTS.register("poseidons_trident_hit", () -> SoundEvent.createVariableRangeEvent(Olympus.of("poseidons_trident_hit")));
 
 }
