60d9a0d14634d6ce9582e5f348724fd5d7c6c90d
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-24 16:39:23 +0200

    ❤️ Network package registrar

--- neoforge/src/main/java/dev/xylonity/olympus/network/OlympusNetwork.java
+++ neoforge/src/main/java/dev/xylonity/olympus/network/OlympusNetwork.java
@@ -1,12 +1,17 @@
 package dev.xylonity.olympus.network;
 
 import dev.xylonity.olympus.network.payload.LightningBoltPayload;
+import dev.xylonity.olympus.network.payload.LyreMusicPayload;
+import dev.xylonity.olympus.network.payload.SoulSalvationPayload;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 
 public final class OlympusNetwork {
 
     public static void registerPayloads(final RegisterPayloadHandlersEvent event) {
-        event.registrar("1").playToClient(LightningBoltPayload.TYPE, LightningBoltPayload.STREAM_CODEC);
+        event.registrar("1")
+                .playToClient(LightningBoltPayload.TYPE, LightningBoltPayload.STREAM_CODEC)
+                .playToClient(LyreMusicPayload.TYPE, LyreMusicPayload.STREAM_CODEC)
+                .playToClient(SoulSalvationPayload.TYPE, SoulSalvationPayload.STREAM_CODEC);
     }
 
 }
