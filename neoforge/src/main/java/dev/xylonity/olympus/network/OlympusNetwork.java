0b35161ba6f259b9db973da764b89dc14c23025a
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-25 19:23:16 +0200

    ♍ Camera shake payload

--- neoforge/src/main/java/dev/xylonity/olympus/network/OlympusNetwork.java
+++ neoforge/src/main/java/dev/xylonity/olympus/network/OlympusNetwork.java
@@ -1,6 +1,7 @@
 package dev.xylonity.olympus.network;
 
 import dev.xylonity.olympus.common.item.HermesSandalsItem;
+import dev.xylonity.olympus.network.payload.CameraShakePayload;
 import dev.xylonity.olympus.network.payload.HermesJumpPayload;
 import dev.xylonity.olympus.network.payload.LightningBoltPayload;
 import dev.xylonity.olympus.network.payload.LyreMusicPayload;
@@ -12,6 +13,7 @@ public final class OlympusNetwork {
 
     public static void registerPayloads(final RegisterPayloadHandlersEvent event) {
         event.registrar("1")
+                .playToClient(CameraShakePayload.TYPE, CameraShakePayload.STREAM_CODEC)
                 .playToClient(LightningBoltPayload.TYPE, LightningBoltPayload.STREAM_CODEC)
                 .playToClient(LyreMusicPayload.TYPE, LyreMusicPayload.STREAM_CODEC)
                 .playToClient(SoulSalvationPayload.TYPE, SoulSalvationPayload.STREAM_CODEC)
