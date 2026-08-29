93e808c34747c2ac44a465eb08b35627c2529428
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-29 22:24:52 +0200

    🏠 Added even more assets + new sounds

--- neoforge/src/main/java/dev/xylonity/olympus/registry/OlympusSounds.java
+++ neoforge/src/main/java/dev/xylonity/olympus/registry/OlympusSounds.java
@@ -17,5 +17,10 @@ public final class OlympusSounds {
     public static final DeferredHolder<SoundEvent, SoundEvent> ARES_SPEAR_SURFACE_HIT = SOUND_EVENTS.register("ares_spear_surface_hit", () -> SoundEvent.createVariableRangeEvent(Olympus.of("ares_spear_surface_hit")));
     public static final DeferredHolder<SoundEvent, SoundEvent> LYRE_MUSIC = SOUND_EVENTS.register("lyre_music", () -> SoundEvent.createVariableRangeEvent(Olympus.of("lyre_music")));
     public static final DeferredHolder<SoundEvent, SoundEvent> HEPHAESTUS_FORGING = SOUND_EVENTS.register("hephaestus_forging", () -> SoundEvent.createVariableRangeEvent(Olympus.of("hephaestus_forging")));
+    public static final DeferredHolder<SoundEvent, SoundEvent> HARPY_DEATH = SOUND_EVENTS.register("harpy_death", () -> SoundEvent.createVariableRangeEvent(Olympus.of("harpy_death")));
+    public static final DeferredHolder<SoundEvent, SoundEvent> HARPY_DASH = SOUND_EVENTS.register("harpy_dash", () -> SoundEvent.createVariableRangeEvent(Olympus.of("harpy_dash")));
+    public static final DeferredHolder<SoundEvent, SoundEvent> HARPY_HIT = SOUND_EVENTS.register("harpy_hit", () -> SoundEvent.createVariableRangeEvent(Olympus.of("harpy_hit")));
+    public static final DeferredHolder<SoundEvent, SoundEvent> HARPY_SHOT = SOUND_EVENTS.register("harpy_shot", () -> SoundEvent.createVariableRangeEvent(Olympus.of("harpy_shot")));
+    public static final DeferredHolder<SoundEvent, SoundEvent> HARPYS_WINGS_FLAPPING = SOUND_EVENTS.register("harpys_wings_flapping", () -> SoundEvent.createVariableRangeEvent(Olympus.of("harpys_wings_flapping")));
 
 }
