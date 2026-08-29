12d11d29a02e2671484f438fad87ac20920b3152
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-29 20:31:57 +0200

    🇷🇺 Additional fields (mostly tick computees) and a couple of tweaks to the harpy

--- neoforge/src/main/java/dev/xylonity/olympus/common/entity/HarpyEntity.java
+++ neoforge/src/main/java/dev/xylonity/olympus/common/entity/HarpyEntity.java
@@ -8,8 +8,14 @@ import com.geckolib.animation.RawAnimation;
 import com.geckolib.animation.object.PlayState;
 import com.geckolib.animation.state.AnimationTest;
 import com.geckolib.util.GeckoLibUtil;
+import dev.xylonity.olympus.common.entity.navigation.HarpyFlyingMoveControl;
+import dev.xylonity.olympus.common.entity.navigation.HarpyFlyingNavigation;
+import dev.xylonity.olympus.common.entity.ai.harpy.internal.DashHarpyGoal;
 import dev.xylonity.olympus.common.entity.ai.harpy.internal.HarpyFlightGoal;
+import dev.xylonity.olympus.common.entity.ai.harpy.internal.HarpyProjectileDodgeGoal;
+import dev.xylonity.olympus.common.entity.ai.harpy.internal.HarpyRetreatGoal;
 import dev.xylonity.olympus.common.entity.ai.harpy.internal.MeleeHarpyGoal;
+import dev.xylonity.olympus.common.entity.ai.harpy.internal.ProjectileHarpyGoal;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
@@ -17,10 +23,9 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.control.FlyingMoveControl;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
-import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.animal.golem.IronGolem;
 import net.minecraft.world.entity.monster.Monster;
@@ -37,11 +42,11 @@ public class HarpyEntity extends Monster implements GeoEntity {
 
     private static final RawAnimation ANIMATION_IDLE = RawAnimation.begin().thenLoop("idle");
     private static final RawAnimation ANIMATION_FLY = RawAnimation.begin().thenLoop("walk");
-    private static final RawAnimation ANIMATION_MELEE = RawAnimation.begin().thenPlayAndHold("attack");
-    private static final RawAnimation ANIMATION_SHOT = RawAnimation.begin().thenPlayAndHold("shot");
-    private static final RawAnimation ANIMATION_DASH_PREPARING = RawAnimation.begin().thenPlayAndHold("dash_preparing");
+    private static final RawAnimation ANIMATION_MELEE = RawAnimation.begin().thenPlay("attack");
+    private static final RawAnimation ANIMATION_SHOT = RawAnimation.begin().thenPlay("shot");
+    private static final RawAnimation ANIMATION_DASH_PREPARING = RawAnimation.begin().thenPlay("dash_preparing");
     private static final RawAnimation ANIMATION_DASH = RawAnimation.begin().thenLoop("dash");
-    private static final RawAnimation ANIMATION_DASH_ENDING = RawAnimation.begin().thenPlay("dash_ending").thenLoop("idle");
+    private static final RawAnimation ANIMATION_DASH_ENDING = RawAnimation.begin().thenPlayAndHold("dash_ending");
 
     public static final int STATE_IDLE = 0;
     public static final int STATE_FLY = 1;
@@ -51,13 +56,23 @@ public class HarpyEntity extends Monster implements GeoEntity {
     public static final int STATE_DASHING = 5;
     public static final int STATE_DASH_ENDING = 6;
 
-    private static final int MELEE_ANIMATION_TICKS = 20;
+    private static final int TICKS_ANIMATION_TRANSITION = 4;
+    private static final int TICKS_MELEE_ANIMATION = 20;
+    private static final int TICKS_SHOT_ANIMATION = 30;
+    private static final int TICKS_SHOT_COOLDOWN = 12 * 20;
+    private static final int TICKS_DASH_PREPARATION = 15 + TICKS_ANIMATION_TRANSITION;
+    private static final int TICKS_DASH_ENDING = 15 + TICKS_ANIMATION_TRANSITION - 2;
+    private static final int TICKS_SPECIAL_ATTACK_CHAIN_DELAY = 60;
 
-    private int attackStateTicks;
+    private static final int TICK_SHOT_RELEASE = 17;
+
+    private static final double SPEED_THRESHOLD = 0.0025D;
+
+    private long specialAttackDelayEndGameTime;
 
     public HarpyEntity(final EntityType<? extends HarpyEntity> type, final Level level) {
         super(type, level);
-        moveControl = new FlyingMoveControl(this, 20, true);
+        moveControl = new HarpyFlyingMoveControl(this);
         setNoGravity(true);
     }
 
@@ -76,8 +91,14 @@ public class HarpyEntity extends Monster implements GeoEntity {
     protected void registerGoals() {
         super.registerGoals();
 
-        goalSelector.addGoal(1, new MeleeHarpyGoal(this, MELEE_ANIMATION_TICKS, 20));
+        goalSelector.addGoal(0, new HarpyProjectileDodgeGoal(this));
+        goalSelector.addGoal(1, new MeleeHarpyGoal(this, TICKS_MELEE_ANIMATION, 20));
+        goalSelector.addGoal(2, new ProjectileHarpyGoal(this, TICKS_SHOT_ANIMATION, TICK_SHOT_RELEASE, TICKS_SHOT_COOLDOWN));
+        goalSelector.addGoal(2, new DashHarpyGoal(this, TICKS_DASH_PREPARATION, TICKS_DASH_ENDING));
+        goalSelector.addGoal(3, new HarpyRetreatGoal(this));
         goalSelector.addGoal(8, new HarpyFlightGoal(this));
+
+        goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 25));
 
         targetSelector.addGoal(1, new HurtByTargetGoal(this, HarpyEntity.class).setAlertOthers());
         targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
@@ -87,7 +108,7 @@ public class HarpyEntity extends Monster implements GeoEntity {
 
     @Override
     protected @NonNull PathNavigation createNavigation(final @NonNull Level level) {
-        final FlyingPathNavigation navigation = new FlyingPathNavigation(this, level);
+        final HarpyFlyingNavigation navigation = new HarpyFlyingNavigation(this, level);
         navigation.setCanOpenDoors(false);
         navigation.setCanFloat(true);
         navigation.setRequiredPathLength(24.0F);
@@ -114,28 +135,33 @@ public class HarpyEntity extends Monster implements GeoEntity {
 
     public void setAttackState(final int state) {
         entityData.set(ATTACK_STATE, state);
-        attackStateTicks = 0;
     }
 
     public int getAttackState() {
         return entityData.get(ATTACK_STATE);
     }
 
+    public boolean canStartSpecialAttack() {
+        return level().getGameTime() >= specialAttackDelayEndGameTime;
+    }
+
+    public void delaySpecialAttacks() {
+        specialAttackDelayEndGameTime = Math.max(specialAttackDelayEndGameTime, level().getGameTime() + TICKS_SPECIAL_ATTACK_CHAIN_DELAY);
+    }
+
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<>("maincontroller", 2, this::mainPredicate));
-        controllers.add(new AnimationController<>("meleecontroller", 2, state ->
+        controllers.add(new AnimationController<>("maincontroller", TICKS_ANIMATION_TRANSITION, this::mainPredicate));
+        controllers.add(new AnimationController<>("meleecontroller", TICKS_ANIMATION_TRANSITION, state ->
                 getAttackState() == STATE_MELEE ? state.setAndContinue(ANIMATION_MELEE) : PlayState.STOP
         ));
 
     }
 
     private PlayState mainPredicate(AnimationTest<HarpyEntity> event) {
-        if (getAttackState() == STATE_IDLE) {
-            event.setAnimation(ANIMATION_IDLE);
-        }
-        else if (getAttackState() == STATE_FLY) {
-            event.setAnimation(ANIMATION_FLY);
+        if (getAttackState() == STATE_IDLE || getAttackState() == STATE_FLY) {
+            final boolean moving = getDeltaMovement().lengthSqr() > SPEED_THRESHOLD;
+            event.setAnimation(moving ? ANIMATION_FLY : ANIMATION_IDLE);
         }
         else if (getAttackState() == STATE_SHOT) {
             event.setAnimation(ANIMATION_SHOT);
