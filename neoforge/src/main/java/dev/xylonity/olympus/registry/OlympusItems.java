fe4ab6692b828d0ed2843e953a2a27424319fd19
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-29 22:32:42 +0200

    🌿 Spawn eggs for both harpies

--- neoforge/src/main/java/dev/xylonity/olympus/registry/OlympusItems.java
+++ neoforge/src/main/java/dev/xylonity/olympus/registry/OlympusItems.java
@@ -28,6 +28,8 @@ public class OlympusItems {
 
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Olympus.MOD_ID);
 
+    public static final DeferredItem<SpawnEggItem> HARPY_SPAWN_EGG = ITEMS.registerItem("harpy_spawn_egg", SpawnEggItem::new, properties -> properties.spawnEgg(OlympusEntities.HARPY.get()));
+    public static final DeferredItem<SpawnEggItem> ELITE_HARPY_SPAWN_EGG = ITEMS.registerItem("elite_harpy_spawn_egg", SpawnEggItem::new, properties -> properties.spawnEgg(OlympusEntities.ELITE_HARPY.get()));
     public static final DeferredItem<BracersOfZeusItem> BRACERS_OF_ZEUS = ITEMS.registerItem("bracers_of_zeus", BracersOfZeusItem::new, properties -> properties.stacksTo(1).rarity(Rarity.EPIC));
     public static final DeferredItem<ArtemisBowItem> BOW_OF_ARTEMIS = ITEMS.registerItem("bow_of_artemis", ArtemisBowItem::new, properties -> properties.durability(384).enchantable(1).rarity(Rarity.EPIC));
     public static final DeferredItem<AphroditeLyreItem> APHRODITE_LYRE = ITEMS.registerItem("aphrodite_lyre", AphroditeLyreItem::new, properties -> properties.stacksTo(1).rarity(Rarity.EPIC));
