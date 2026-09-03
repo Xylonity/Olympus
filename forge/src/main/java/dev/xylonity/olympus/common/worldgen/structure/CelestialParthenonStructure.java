b4955a69f60ffa8c887386be5f96bcf31052b223
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-09-03 18:31:22 +0200

    📦 Port parthenon structure set

--- forge/src/main/java/dev/xylonity/olympus/common/worldgen/structure/CelestialParthenonStructure.java
+++ forge/src/main/java/dev/xylonity/olympus/common/worldgen/structure/CelestialParthenonStructure.java
@@ -1,7 +1,6 @@
 package dev.xylonity.olympus.common.worldgen.structure;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import dev.xylonity.olympus.registry.OlympusStructureTypes;
 import net.minecraft.core.BlockPos;
@@ -13,34 +12,30 @@ import net.minecraft.world.level.levelgen.WorldGenerationContext;
 import net.minecraft.world.level.levelgen.heightproviders.HeightProvider;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
-import net.minecraft.world.level.levelgen.structure.pools.DimensionPadding;
 import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
-import net.minecraft.world.level.levelgen.structure.pools.alias.PoolAliasLookup;
-import net.minecraft.world.level.levelgen.structure.structures.JigsawStructure;
-import net.minecraft.world.level.levelgen.structure.templatesystem.LiquidSettings;
 import org.jspecify.annotations.NonNull;
 
 import java.util.Optional;
 
 public final class CelestialParthenonStructure extends Structure {
 
-    public static final MapCodec<CelestialParthenonStructure> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
+    public static final Codec<CelestialParthenonStructure> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             settingsCodec(instance),
             StructureTemplatePool.CODEC.fieldOf("start_pool").forGetter(structure -> structure.startPool),
             Codec.intRange(0, 20).fieldOf("size").forGetter(structure -> structure.maxDepth),
             HeightProvider.CODEC.fieldOf("start_height").forGetter(structure -> structure.startHeight),
             Codec.BOOL.fieldOf("use_expansion_hack").forGetter(structure -> structure.expansion),
-            JigsawStructure.MaxDistance.CODEC.fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter)
+            Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter)
     ).apply(instance, CelestialParthenonStructure::new));
 
     private final Holder<StructureTemplatePool> startPool;
     private final int maxDepth;
     private final HeightProvider startHeight;
     private final boolean expansion;
-    private final JigsawStructure.MaxDistance maxDistanceFromCenter;
+    private final int maxDistanceFromCenter;
 
-    public CelestialParthenonStructure(StructureSettings settings, Holder<StructureTemplatePool> startPool, int maxDepth, HeightProvider startHeight, boolean expansion, JigsawStructure.MaxDistance maxDistanceFromCenter) {
+    public CelestialParthenonStructure(StructureSettings settings, Holder<StructureTemplatePool> startPool, int maxDepth, HeightProvider startHeight, boolean expansion, int maxDistanceFromCenter) {
         super(settings);
         this.startPool = startPool;
         this.maxDepth = maxDepth;
@@ -54,7 +49,7 @@ public final class CelestialParthenonStructure extends Structure {
         final ChunkPos chunkPos = context.chunkPos();
         final int y = this.startHeight.sample(context.random(), new WorldGenerationContext(context.chunkGenerator(), context.heightAccessor()));
         final BlockPos start = new BlockPos(chunkPos.getMinBlockX(), y, chunkPos.getMinBlockZ());
-        return JigsawPlacement.addPieces(context, this.startPool, Optional.empty(), this.maxDepth, start, this.expansion, Optional.empty(), this.maxDistanceFromCenter, PoolAliasLookup.EMPTY, DimensionPadding.ZERO, LiquidSettings.APPLY_WATERLOGGING);
+        return JigsawPlacement.addPieces(context, this.startPool, Optional.empty(), this.maxDepth, start, this.expansion, Optional.empty(), this.maxDistanceFromCenter);
     }
 
     @Override
